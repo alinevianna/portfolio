4c55b75e7190c3e564668243760d8238a9dc0d6c
Aline Vianna <aliine@live.jp>
2024-04-28 21:06:06 -0300

    atualizacao do botao dark mode, correcao bugs

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const $html = document.querySelector('html');
 const $checkbox = document.querySelector('#switch');
+const foto = document.querySelector('.title__profile')
 
 $checkbox.addEventListener('change', function() {
     $html.classList.toggle('dark-mode');
+    if ($html.className == 'dark-mode') {
+        foto.setAttribute('src', `assets/profile1.jpg`)
+    } else {
+        foto.setAttribute('src', `assets/profile2.jpg`)
+    }
 })
